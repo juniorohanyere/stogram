e113ab769de27886526d1dd6b7245c00e335f08e
Twin J <junohanyere@gmail.com>
2023-09-06 00:58:27 +0100

    function to change into another directory

---
+++ core/filesystem/changedir.c
@@ -0,0 +1,37 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "changedir.h"
+#include "array.h"
+
+/**
+ * change_dir - changes the current working directory of a user to the
+ *		directory specified
+ *
+ * @win: the current window
+ * @command: the command found in the executable that brought about this
+ *	     function
+ * @args: a list of command line arguments
+ *
+ * Return: return 0 on success
+ *	   return 1 on failure
+*/
+
+int change_dir(WINDOW *win, char *command, char **args)
+{
+	if (a_strlen(args) == 2)
+	{
+		if (access(args[1], F_OK) == 0)
+		{
+			/* convert hexadecimal to oringinal string */
+
+			if (setenv("_PWD", args[1], 1) == -1)
+			{
+				dprintf(STDERR_FILENO, "An error occurred\n");
+				return (1);
+			}
+		}
+	}
+	return (0);
+}
---
+++ core/filesystem/changedir.h
@@ -0,0 +1,8 @@
+#ifndef CHANGEDIR_H
+#define CHANGEDIR_H
+
+#include <ncurses.h>
+
+int change_dir(WINDOW *win, char *command, char **args);
+
+#endif	/* CHANGEDIR_H */
