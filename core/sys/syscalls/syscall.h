f5fbdac7df8390be417c33dadb86d9e623f99a33
Twin J <junohanyere@gmail.com>
2023-09-13 13:12:02 +0100

    restructuring

--- core/sys/syscalls/syscall.h
+++ core/sys/syscalls/syscall.h
@@ -1,36 +1,48 @@
-#ifndef EXECUTABLES_H
-#define EXECUTABLES_H
+#ifndef SYSCALL_H
+#define SYSCALL_H
 
 #include "syspath.h"
 
-#define LS "6c73"
-#define CD "6364"
-#define MKDIR "6d6b646972"
-#define MV "6d76"
-#define RM "726d"
-#define CAT "636174"
-#define ECHO "6563686f"
-#define ENV "656e76"
-#define XSH "787368"
-#define RMDIR "726d646972"
+/**
+ * these are macros to system calls, the below system calls are hexadecimal
+ * representation of the common unix system calls
+ *
+ * Note: some system calls here might differ from the common unix system calls
+ *	 and not all the unix system calls might be used, and there might be
+ *	 introduction of new system calls that does not exit in the common
+ *	 system calls
+*/
+
+#define _OPEN		"6f70656e"
+#define _READ		"7265616"
+#define _CLOSE		"636c6f7365"
+#define _EXIT		"65786974"
+#define _WRITE		"7772697465"
+#define _FORK		"666f726b"
+#define _STAT		"73746174"
+#define _CHMOD		"63686d6f64"
+#define _CHOWN		"63686f776e"
+#define _READDIR	"72656164646972"
+#define _CLOSEDIR	"636c6f7365646972"
+#define _SEEK		"7365656b"
 
 /**
- * struct command_s - structure for mapping commands with the right function
+ * struct syscall_s - structure for mapping commands with the right routines
  *
  * @cmd: the command to execute
- * @func: the function to run for @cmd
+ * @func: the routine to run for @cmd
  *
  * Return: return nothing
 */
 
-typedef struct command_s
+typedef struct syscall_s
 {
 	char *cmd;
 	int (*func)(char *, char **);
-} command_t;
+} syscall_t;
 
-int execute_command(char *command, char *args[]);
-int locate_command(char *args[], char *home);
-int locate_command2(path_t *path, char *args[], char *home);
+int _exec(char *command, char *args[]);
+int route(char *args[], char *home);
+int route2(path_t *path, char *args[], char *home);
 
 #endif	/* EXEXUTABLES_H */
