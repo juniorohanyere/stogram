ee8fde812dc484daa837ef62453a0a970766b839
Twin J <junohanyere@gmail.com>
2023-09-21 16:26:14 +0100

    deleted

--- core/exit.h
+++
@@ -1,7 +0,0 @@
-#ifndef EXIT_H
-#define EXIT_H
-
-int exit_st(char *buffer);
-void clean_up(void);
-
-#endif	/* EXIT_H */
--- core/logo.h
+++
@@ -1,6 +0,0 @@
-#ifndef LOGO_H
-#define LOGO_H
-
-void logo_stg(int y, int x);
-
-#endif	/* LOGO_H */
