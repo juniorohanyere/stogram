e29aeda224e179e11c0c88841cadc0285f0889d3
Twin J <junohanyere@gmail.com>
2023-10-01 12:10:26 +0100

    genarating a library file SKERNEL

--- core/tests/test.sh
+++ core/tests/test.sh
@@ -2,6 +2,4 @@
 
 export LD_LIBRARY_PATH=/home/twinj/juniorohanyere/stogram/core:$LD_LIBRARY_PATH
 
-valgrind --leak-check=full --track-origins=yes --show-leak-kinds=all ./test
-
-./test
+valgrind --leak-check=full --track-origins=yes --show-leak-kinds=all tests/test
